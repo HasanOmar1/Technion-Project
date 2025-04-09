1a96315784ee2755de47d9ae49ed08d9c1e541bc
Hasan Omar <r3domar1@gmail.com>
2025-04-09 10:57:50 +0300

    added

---
+++ week3/week3.js
@@ -0,0 +1,23 @@
+// let score = 75;
+// let grade;
+
+// if (score >= 90) {
+//   grade = "A";
+// } else if (score >= 80) {
+//   grade = "B";
+// } else if (score >= 70) {
+//   grade = "C";
+// } else if (score >= 60) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+
+// const answer = score >= 90 ? "A" : score >= 80 ? "B" : score >= 60 ? "D" : "F";
+// console.log("הציון הוא:", answer);
+
+let num = prompt("Enter num");
+
+if (num > 0) alert(`${num} is bigger than 0`);
+else if (num < 0) alert(`${num} is smaller than 0`);
+else alert(`${num} is equal 0`);
