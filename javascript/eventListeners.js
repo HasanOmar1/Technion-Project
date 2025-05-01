fa4d4467797db7d09907a9449bb2758df206f183
HasanOmar1 <r3domar1@gmail.com>
2025-05-01 12:11:03 +0300

    removed 'eventlistener.js'

--- javascript/eventListeners.js
+++
@@ -1,6 +0,0 @@
-// "use strict";
-
-// import { addContactOpenMenu, closeMenu, addFormMenu } from "./domVariables.js";
-
-// addContactOpenMenu.addEventListener("click", () => showMenu(addFormMenu));
-// closeMenu.addEventListener("click", () => hideMenu(addFormMenu));
